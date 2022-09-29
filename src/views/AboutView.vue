4b73fd2a2c887ea0311ba929162747abe9b030c2
Clayton0205 <claytonxxclayton@gmail.com>
2022-09-29 09:30:39 +0200

    More Than Tuesday

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Clayton's To-Do App</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At reiciendis dolorum explicabo qui, facilis facere impedit fugit ex consectetur molestiae.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At reiciendis dolorum explicabo qui, facilis facere impedit fugit ex consectetur molestiae.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At reiciendis dolorum explicabo qui, facilis facere impedit fugit ex consectetur molestiae.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At reiciendis dolorum explicabo qui, facilis facere impedit fugit ex consectetur molestiae.</p>
   </div>
 </template>
