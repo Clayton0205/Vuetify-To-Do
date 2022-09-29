fda30276fbcb34ec5293c38bc86b966c74c61920
Clayton0205 <claytonxxclayton@gmail.com>
2022-09-29 10:30:53 +0200

    Add Task

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,75 +1,74 @@
 <template>
   <div class="home">
-    <v-list
-      flat
-      class="pt-0"
-    >
-   <div v-for="task in tasks" :key="task.id">
-    <v-list-item
-    @click="doneTask(task.id)"
-    :class=" {'blue' : task.done} " 
-    >
+    <v-text-field v-model="newTaskTitle" @click:append="addTask" @keyup.enter="addTask" class="pt-5" outlined label="Add Task" append-icon="mdi-plus" hide-details clearable></v-text-field>
+    <v-list flat class="pt-0">
+      <div v-for="task in tasks" :key="task.id">
+        <v-list-item @click="doneTask(task.id)" :class=" {'blue' : task.done} ">
           <template v-slot:default>
             <v-list-item-action>
-              <v-checkbox
-                :input-value="task.done"
-                color="primary"
-              ></v-checkbox>
+              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
-              <v-list-item-title
-              :class="{ 'text-decoration-line-through' : task.done }"
-              >{{task.title}}</v-list-item-title>
+              <v-list-item-title :class="{ 'text-decoration-line-through' : task.done }">{{task.title}}
+              </v-list-item-title>
             </v-list-item-content>
             <v-list-item-action>
-          <v-btn 
-          @click.stop="deleteTask(task.id)"
-          icon>
-            <v-icon color="red">mdi-delete-outline</v-icon>
-          </v-btn>
-        </v-list-item-action>
+              <v-btn @click.stop="deleteTask(task.id)" icon>
+                <v-icon color="red">mdi-delete-outline</v-icon>
+              </v-btn>
+            </v-list-item-action>
           </template>
         </v-list-item>
         <v-divider></v-divider>
-   </div>
+      </div>
     </v-list>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        tasks: [
-          {
-            id: 1,
-            title: "Play League",
-            done: false,
-          },
-          {
-            id: 2,
-            title: "Play Ruined King",
-            done: false,
-          },
-          {
-            id: 3,
-            title: "Work on Portfolio",
-            done: false,
-          }
-        ]
-      }
+export default {
+  data() {
+    return {
+      newTaskTitle : "",
+      tasks: [
+        // {
+        //   id: 1,
+        //   title: "Play League",
+        //   done: false,
+        // },
+        // {
+        //   id: 2,
+        //   title: "Play Ruined King",
+        //   done: false,
+        // },
+        // {
+        //   id: 3,
+        //   title: "Work on Portfolio",
+        //   done: false,
+        // }
+      ]
+    }
+  },
+  methods: {
+    addTask() {
+     let newTask = {
+      id: Date.now(),
+      title: this.newTaskTitle,
+      done: false
+     }
+     this.tasks.push(newTask)
+     this.newTaskTitle = ""
     },
-    methods: {
-      doneTask(id) {
-        let task = this.tasks.filter( task => task.id === id)[0]
-        task.done = !task.done 
-      },
-      deleteTask(id) {
-        this.tasks = this.tasks.filter(task => task.id !== id)
-      }
+    doneTask(id) {
+      let task = this.tasks.filter(task => task.id === id)[0]
+      task.done = !task.done
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter(task => task.id !== id)
     }
   }
+}
 </script>
 
 <style scoped>
